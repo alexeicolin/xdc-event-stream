f75c64519377b1d1a9cc38154cc5fa4ce0af5199
Alexei Colin <ac@alexeicolin.com>
2015-01-03 13:47:46 -0500

    eventgenapp: add System_printf out output
    This is to highlight that event log is altogether different
    from the system "standard out" output.

--- eventgenapp/eventgen.c
+++ eventgenapp/eventgen.c
@@ -1,12 +1,15 @@
 #include <xdc/std.h>
-#include <eventgen/EventGen.h>
+#include <xdc/runtime/System.h>
 
-#include <xdc/cfg/global.h>
+#include <eventgen/EventGen.h>
 
 /* A simple app for playing around with log events */
 
 Int main(Int argc, Char* argv[])
 {
+    System_printf("Hello world! Generating a log event...\n");
+
     EventGen_genEvent(42);
+
     return 0;
 }
